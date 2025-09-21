c391cf28823388216e87514eedb90c3759b52197
Zulaikha Zakiullah <zzakiullah1@gmail.com>
2025-09-21 10:32:22 -0400

    Add some libraries

---
+++ src/app/datasets/page.tsx
@@ -0,0 +1,14 @@
+export default function Datasets() {
+  return (
+    <div
+      className="font-sans flex flex-col md:flex-row items-center justify-center w-full min-h-screen"
+    >
+      <nav className="flex flex-col w-full md:w-1/3" style={{ border: "1px solid red" }}>
+        <input />
+      </nav>
+      <main className="flex flex-col w-full md:w-2/3" style={{ border: "1px solid blue" }}>
+
+      </main>
+    </div>
+  );
+}
